f666956977c157734d809499b8f607bcba8feffd
Chourot Nicolas <nicolaschourot@videotron.ca>
2021-04-12 07:50:49 -0400

    UI icons

--- Models/DBEntities.Designer.cs
+++ Models/DBEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Nicolas.Chourot\Desktop\Post-it project -3\Models\DBEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Nicolas.Chourot\Desktop\Post-it project -4\Models\DBEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
